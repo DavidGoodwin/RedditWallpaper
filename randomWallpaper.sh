bbfa2b0c2cafebe930dcd02ce5fb731f5c2ac5b6
Alfred456654 <augiraud@gmail.com>
2014-10-08 17:10:27 +0200

    automatic guess of desktop environment

--- randomWallpaper.sh
+++ randomWallpaper.sh
@@ -8,19 +8,56 @@
 # Wallpapers directory
 WALLS=$HOME/Pictures/todayswalls
 
+# Nitrogen configuration file
+NITROGEN_CFG=$HOME/.config/nitrogen/bg-saved.cfg
+
 # Tool to change the wallpaper
 # Supported values:
 #   nitrogen
 #   gsettings (for GNOME 3)
 #   feh
-#   xfconf-query
-TOOL="nitrogen"
+#   xfconf-query (for XFCE 4)
+TOOL=""
 
 # Number of screens
 NB_SCREENS=2
 
 ## END CONFIGURATION
 
+
+function guess_env {
+    case "${DESKTOP_SESSION}" in
+        *box )
+            if [ -d "${NITROGEN_CFG%\/*}" ]; then
+                if [ ! -z "$(command -v nitrogen)" ]; then
+                    echo "nitrogen"
+                fi
+            elif [ ! -z "$(command -v feh)" ]; then
+                echo "feh"
+            fi
+            ;;
+        gnome* )
+            if [ ! -z "$(command -v gsettings)" ]; then
+                echo "gsettings"
+            fi
+            ;;
+        xfce*|Xfce* )
+            if [ ! -z "$(command -v xfconf-query)" ]; then
+                echo "xfconf-query"
+            fi
+            ;;
+        * )
+            if [ ! -z "$(command -v zenity)" ]; then
+                zenity --list --title='What tool do you want to use?' --column='select' --radiolist --column='tool' --column='label' --hide-column=2 \
+                    FALSE 'nitrogen' 'nitrogen' \
+                    FALSE 'feh' 'feh' \
+                    FALSE 'gsettings' 'gsettings (GNOME)' \
+                    FALSE 'xfconf-query' 'xfconf-query (XFCE)'
+            elif [ ! -z "$(command -v kdialog)" ]; then
+                kdialog --radiolist 'What tool do you want to use?' 'nitrogen' 'nitrogen' off 'feh' 'feh' off 'gsettings' 'gsettings (GNOME)' off 'xfconf-query' 'xfconf-query (XFCE)' off 2>/dev/null
+            fi
+    esac
+}
 
 # Check whether the number of screens provided is an integer
 if [ -z "$(echo ${NB_SCREENS} | grep -E '^[0-9]*$')" ]; then
@@ -40,6 +77,9 @@ if [ -z "$(find ${WALLS} -type f)" ]; then
     exit 1
 fi
 
+# Try to guess desktop environment
+TOOL="$(guess_env)"
+
 # Check if the chosen tool is installed
 if [ -z "$(command -v ${TOOL})" ]; then
     echo "ERROR: ${TOOL} does not seem to be installed, or ${TOOL} is not present in \$PATH" > /dev/stderr
@@ -50,18 +90,15 @@ fi
 # Change wallpaper using nitrogen
 function wall_nitrogen {
 
-    # Nitrogen configuration file
-    WALL_CFG=$HOME/.config/nitrogen/bg-saved.cfg
-
     # Check nitrogen configuration folder
-    if [ ! -d "${WALL_CFG%\/*}" ]; then
-        echo "ERROR: directory ${WALL_CFG} does not exist" > /dev/stderr
+    if [ ! -d "${NITROGEN_CFG%\/*}" ]; then
+        echo "ERROR: directory ${NITROGEN_CFG} does not exist" > /dev/stderr
         exit 2
     fi
 
     # Empty previous nitrogen configuration
-    rm -f "${WALL_CFG}"
-    touch "${WALL_CFG}"
+    rm -f "${NITROGEN_CFG}"
+    touch "${NITROGEN_CFG}"
 
     # Loop through the screens
     for i in $(seq 1 ${NB_SCREENS}); do
@@ -71,11 +108,11 @@ function wall_nitrogen {
         wall="$(basename ${wall})"
 
         # Set first wallpaper
-        echo "[xin_$((i-1))]" >> "${WALL_CFG}"
-        echo "file=${WALLS}/${wall}" >> "${WALL_CFG}"
-        echo "mode=0" >> "${WALL_CFG}"
-        echo "bgcolor=#000000" >> "${WALL_CFG}"
-        echo "" >> "${WALL_CFG}"
+        echo "[xin_$((i-1))]" >> "${NITROGEN_CFG}"
+        echo "file=${WALLS}/${wall}" >> "${NITROGEN_CFG}"
+        echo "mode=0" >> "${NITROGEN_CFG}"
+        echo "bgcolor=#000000" >> "${NITROGEN_CFG}"
+        echo "" >> "${NITROGEN_CFG}"
     done
 
     nitrogen --restore
